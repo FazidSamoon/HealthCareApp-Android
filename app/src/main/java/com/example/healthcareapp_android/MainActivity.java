70abe2ed8a1051339e1c36a074e6aaab69ffbf0c
Fazid Samoon <tuan.fazid@outlook.com>
2022-09-16 12:19:20 +0530

    user profile view created

--- app/src/main/java/com/example/healthcareapp_android/MainActivity.java
+++ app/src/main/java/com/example/healthcareapp_android/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.healthcareapp_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_user_profile);
+
+
     }
 }
