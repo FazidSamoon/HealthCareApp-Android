e8b3715a5fe9892dfec47627747cca4ca12e29f7
Fazid Samoon <tuan.fazid@outlook.com>
2022-09-18 21:44:57 +0530

    meal plan ui created

---
+++ app/src/main/java/com/example/healthcareapp_android/activity/Create_meal_plan.java
@@ -0,0 +1,14 @@
+package com.example.healthcareapp_android.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Create_meal_plan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_meal_plan);
+    }
+}
---
+++ app/src/main/java/com/example/healthcareapp_android/activity/Edit_meal_plan.java
@@ -0,0 +1,14 @@
+package com.example.healthcareapp_android.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Edit_meal_plan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_meal_plan);
+    }
+}
