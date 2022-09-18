6f0627c58fc71a3ed7755d7d3530822062dccdce
Fazid Samoon <tuan.fazid@outlook.com>
2022-09-18 17:55:12 +0530

    blog uis created

---
+++ app/src/main/java/com/example/healthcareapp_android/activity/User_Blog_Activities.java
@@ -0,0 +1,14 @@
+package com.example.healthcareapp_android.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class User_Blog_Activities extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_blog_activities);
+    }
+}
